79b30f2cf6b4c728f1dcf13492059580e4812d32
Julien D <djju83550@gmail.com>
2023-03-28 17:52:39 +0200

    avancee activitee publication

---
+++ app/src/main/java/com/example/blablaplane/Publish_PriceVehicule_Activity.java
@@ -0,0 +1,14 @@
+package com.example.blablaplane;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Publish_PriceVehicule_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_activity_publish_price_vehicule);
+    }
+}
