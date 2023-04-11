c1797bbe84b23fd3113e46637137978031f4d611
guillaumeArrigoni <guillaume.arr@gmail.com>
2023-04-11 16:55:53 +0200

    S1 - Refactor Name File #1

--- app/src/main/java/com/example/blablaplane/InscriptionPage.java
+++ app/src/main/java/com/example/blablaplane/InscriptionPage.java
@@ -10,7 +10,7 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 
-public class MainActivity extends AppCompatActivity {
+public class InscriptionPage extends AppCompatActivity {
 
 
 
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.inscription_page);
 
         isPiloteCheckBox = findViewById(R.id.checkBoxInscription);
         AirplaneCheckBox = findViewById(R.id.subCheckBoxInscriptionAirplane);
