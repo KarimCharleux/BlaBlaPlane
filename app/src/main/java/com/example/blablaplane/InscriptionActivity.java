6f5eb0ec02d24c13ec0e677d5b937caa2d47c54e
guillaumeArrigoni <guillaume.arr@gmail.com>
2023-05-16 15:25:20 +0200

    S1 - Update InscriptionActivity.java #10

--- app/src/main/java/com/example/blablaplane/InscriptionActivity.java
+++ app/src/main/java/com/example/blablaplane/InscriptionActivity.java
@@ -3,6 +3,11 @@ package com.example.blablaplane;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.text.TextWatcher;
+import android.text.Editable;
+import android.widget.TextView;
+
 
 public class InscriptionActivity extends AppCompatActivity {
 
@@ -10,5 +15,87 @@ public class InscriptionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inscription);
+
+
+        EditText firstName = findViewById(R.id.firstName);
+        EditText lastName = findViewById(R.id.lastName);
+        EditText emailAdress = findViewById(R.id.EmailAddress);
+        EditText password = findViewById(R.id.Password);
+        EditText repeatPassword = findViewById(R.id.RepeatPassword);
+
+        firstName.addTextChangedListener(new TextWatcher() {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String userFirstName = firstName.getText().toString();
+            }
+        });
+
+        lastName.addTextChangedListener(new TextWatcher() {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String userLastName = lastName.getText().toString();
+            }
+        });
+
+
+        emailAdress.addTextChangedListener(new TextWatcher() {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String userEmailAdress = emailAdress.getText().toString();
+            }
+        });
+
+
+        password.addTextChangedListener(new TextWatcher() {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String userPassword = password.getText().toString();
+            }
+        });
+
+
+        repeatPassword.addTextChangedListener(new TextWatcher() {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String userRepeatPassword = repeatPassword.getText().toString();
+            }
+        });
     }
+
+
 }
