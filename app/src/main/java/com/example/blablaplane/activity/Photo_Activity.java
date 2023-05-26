a1be520690dd191bce09e90ab43c8c6f8573ee72
guillaumeArrigoni <guillaume.arr@gmail.com>
2023-05-26 16:05:44 +0200

    S1 - Create starter photo view #49

---
+++ app/src/main/java/com/example/blablaplane/activity/Photo_Activity.java
@@ -0,0 +1,17 @@
+package com.example.blablaplane.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.blablaplane.R;
+
+public class Photo_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo);
+    }
+
+}
