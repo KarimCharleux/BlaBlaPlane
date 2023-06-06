8e26169094cdf626aed5728950823cbbd043b6e3
KarimCharleux <karim.charleux@etu.univ-cotedazur.fr>
2023-06-06 22:25:40 +0200

    Remove AircraftArray.java because it's in database now

--- app/src/main/java/com/example/blablaplane/activity/PublishPriceAircraftActivity.java
+++ app/src/main/java/com/example/blablaplane/activity/PublishPriceAircraftActivity.java
@@ -1,14 +1,21 @@
 package com.example.blablaplane.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.blablaplane.R;
+import com.example.blablaplane.object.DataBase;
 import com.example.blablaplane.object.aircraft.AircraftAdapter;
 import com.example.blablaplane.object.aircraft.AircraftAdapterListener;
-import com.example.blablaplane.object.aircraft.AircraftArray;
+import com.example.blablaplane.object.user.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 public class PublishPriceAircraftActivity extends AppCompatActivity implements AircraftAdapterListener {
 
@@ -17,20 +24,37 @@ public class PublishPriceAircraftActivity extends AppCompatActivity implements A
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_publish_price_aircraft);
 
-        // Get the list of trips
-        AircraftArray vehiculeArray = AircraftArray.getInstance();
+        String userID = getSharedPreferences("user_data", Context.MODE_PRIVATE).getString("user_id", null);
 
-        // Create the adapter
-        AircraftAdapter vehiculeAdapter = new AircraftAdapter(getApplicationContext(), vehiculeArray);
+        // Get the list of its aircraft
+        DatabaseReference userRef = DataBase.USERS_REFERENCE.child(userID);
 
-        // Retrieve the list of trips
-        ListView vehiculeList = findViewById(R.id.LV_aircraft);
+        // Fetch the user from the database
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // Get the user object
+                User user = snapshot.getValue(User.class);
+                assert user != null;
 
-        // Set the adapter
-        vehiculeList.setAdapter(vehiculeAdapter);
+                // Create the adapter
+                AircraftAdapter aircraftAdapter = new AircraftAdapter(getApplicationContext(), user.getAircraftList());
 
-        // Set the listener
-        vehiculeAdapter.setListener(this);
+                // Retrieve the list of trips
+                ListView vehiculeList = findViewById(R.id.LV_aircraft);
+
+                // Set the adapter
+                vehiculeList.setAdapter(aircraftAdapter);
+
+                // Set the listener
+                aircraftAdapter.setListener(PublishPriceAircraftActivity.this);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                System.err.println("Error while fetching user from the database");
+            }
+        });
     }
 
     @Override
